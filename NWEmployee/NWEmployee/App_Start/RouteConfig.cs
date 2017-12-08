07bffce4148c847fa8433286e5a17f8ec458bc4f
jharris1829 <jharris1829@gmail.com>
2017-12-08 00:57:05 -0700

    Final Draft

--- NWEmployee/NWEmployee/App_Start/RouteConfig.cs
+++ NWEmployee/NWEmployee/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NWEmployee
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Labs", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- NWEmployee/NWEmployee/Controllers/AccountController.cs
+++ NWEmployee/NWEmployee/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace NWEmployee.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Labs");
         }
 
         //
