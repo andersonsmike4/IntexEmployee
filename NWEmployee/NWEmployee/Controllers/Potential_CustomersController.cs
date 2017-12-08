c08b069fa58a2a72ce28aca52377accd68ea3592
andersonsmike4 <andersonsmike4@gmail.com>
2017-12-08 00:05:42 -0700

    scaffolded items and main views

---
+++ NWEmployee/NWEmployee/Controllers/Potential_CustomersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NWEmployee.DAL;
+using NWEmployee.Models;
+
+namespace NWEmployee.Controllers
+{
+    public class Potential_CustomersController : Controller
+    {
+        private NorthwestContext db = new NorthwestContext();
+
+        // GET: Potential_Customers
+        public ActionResult Index()
+        {
+            return View(db.p_customers.ToList());
+        }
+
+        // GET: Potential_Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Potential_Customers potential_Customers = db.p_customers.Find(id);
+            if (potential_Customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(potential_Customers);
+        }
+
+        // GET: Potential_Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Potential_Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "potentialID,companyName,ContactName,contactPhone,contactEmail,notes")] Potential_Customers potential_Customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.p_customers.Add(potential_Customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(potential_Customers);
+        }
+
+        // GET: Potential_Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Potential_Customers potential_Customers = db.p_customers.Find(id);
+            if (potential_Customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(potential_Customers);
+        }
+
+        // POST: Potential_Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "potentialID,companyName,ContactName,contactPhone,contactEmail,notes")] Potential_Customers potential_Customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(potential_Customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(potential_Customers);
+        }
+
+        // GET: Potential_Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Potential_Customers potential_Customers = db.p_customers.Find(id);
+            if (potential_Customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(potential_Customers);
+        }
+
+        // POST: Potential_Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Potential_Customers potential_Customers = db.p_customers.Find(id);
+            db.p_customers.Remove(potential_Customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NWEmployee/NWEmployee/Models/Invoices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWEmployee.Models
+{
+    [Table("Invoices")]
+    public class Invoices
+    {
+        [Key]
+        public int invoiceNo { get; set; }
+
+        [Required]
+        [DisplayName("Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime? paymentDueDate { get; set; }
+
+        [Required]
+        [DisplayName("Early Payment Date")]
+        [DataType(DataType.Date)]
+        public DateTime? earlyPaymentDate { get; set; }
+
+        [Required]
+        [DisplayName("Early Payment Discount")]
+        public decimal? earlyPaymentDiscount { get; set; }
+
+        [Required]
+        [DisplayName("Date Mailed")]
+        [DataType(DataType.Date)]
+        public DateTime? mailedDate { get; set; }
+
+        [Required]
+        [DisplayName("Total Payment")]
+        public decimal? totalPayment { get; set; }
+
+        [Required]
+        [DisplayName("Total Paid")]
+        public decimal? totalPaid { get; set; }
+
+        [Required]
+        [DisplayName("Balance")]
+        public decimal? balance { get; set; }
+
+        [Required]
+        [DisplayName("One Time Discount")]
+        public decimal? oneTimeDiscount { get; set; }
+    }
+}
